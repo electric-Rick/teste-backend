d387dbf86bce8d4ee32218be32fcd8fb515c76f0
electric-rick <erick.augusto.ebs@gmail.com>
2022-05-09 17:11:56 -0300

    Adição de alguns comentários no documento.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,14 @@
+# Author: Erick Augusto 
+# Last time modified: 9/05/2022
+# Notes: 
+#   cpf_cnpj são os novos campos que devem apenas aceitar números.
+#   
+#-------------------------------------------------
+
 class Contact < ApplicationRecord
     validates :name,  length: {minimum:4}
     validates :active, :birthday, :email, :mobile, :advertising, presence: true
+    
+    # Validates the CPF and CNPJ without - and /. 
     validates :cpf_cnpj, :numericality => {:only_integer => true}
 end
