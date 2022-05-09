0e5a656e6cd6afc8027e37f6af8e8ea0c6e3d159
electric-rick <erick.augusto.ebs@gmail.com>
2022-05-09 14:42:22 -0300

    Correção da nomenclatura do vaildador

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ApplicationRecord
     validates :name,  length: {minimum:4}
     validates :active, :birthday, :email, :mobile, :advertising, presence: true
-    validates :cpf_cnpj, :numerically => {:only_integer => true}
+    validates :cpf_cnpj, :numericality => {:only_integer => true}
 end
