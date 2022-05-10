bcf993f919490b6afe9f832fb64ac8bc1d5e187f
electric-rick <erick.augusto.ebs@gmail.com>
2022-05-10 19:12:22 -0300

    Reconfiguração do kafka nos initializers

--- config/initializers/kafka.rb
+++ config/initializers/kafka.rb
@@ -1,10 +1,13 @@
-
+require 'kafka'
 puts "Kafka | Environment: #{Rails.env}"
+
+$kafka = Kafka.new( seed_brokers: ["kafka://127.0.0.1:9092"],
+                    logger: Rails.logger)
 
 DeliveryBoy.configure do |config|
     config.brokers = YAML.load_file(File.join(Rails.root, "config", "kafka.yml"))[Rails.env]["brokers"]
 end
 
 Racecar.configure do |config|
-    config.brokers = YAML.load_file(File.join(Rails.root, "config", "kafka.yml"))[Rails.env]["brokers"]
+    config.brokers = YAML.load_file(File.join(Rails.root, "config", "racecar.yml"))[Rails.env]["brokers"]
 end
