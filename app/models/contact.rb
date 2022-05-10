a64978d6dce05498a75ce10346bd7c382a10ba4b
electric-rick <erick.augusto.ebs@gmail.com>
2022-05-10 18:57:55 -0300

    Algumas modificações foram feitas neste arquivo, mas ainda necessita criar um método que salve os dados gerados por este modelo em um banco de dados não relacional.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,14 +1,14 @@
 # Author: Erick Augusto 
-# Last time modified: 9/05/2022
+# Last time modified: 10/05/2022 17:20:50
 # Notes: 
 #   cpf_cnpj são os novos campos que devem apenas aceitar números.
 #   
 #-------------------------------------------------
 
-class Contact < ApplicationRecord
+class Contact < ActiveRecord::Base    
     validates :name,  length: {minimum:4}
     validates :active, :birthday, :email, :mobile, :advertising, presence: true
     
-    # Validates the CPF and CNPJ without - and /. 
+   # Validates the CPF and CNPJ without - and /. 
     validates :cpf_cnpj, :numericality => {:only_integer => true}
 end
